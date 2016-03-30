3056871c822a420099fedf38f3db6d742db7b989
Erich Stark <stark.erich72@gmail.com>
2016-03-30 23:44:17 +0200

    added option to set simulation parameters from web.

--- Sikmy_vrh_par.m
+++ Sikmy_vrh_par.m
@@ -1,32 +1,31 @@
-% Parametre pre model so sikmym vrhom
-v0      = 50;    % [m/s] pociatocna rychlost v smere hodu
-alfa_deg= 60;    % [deg] uhol vrhu v stupnoch
+function [] = Sikmy_vrh_par(v0, alfa_deg)
+    % Parametre pre model so sikmym vrhom
+    %v0 - [m/s] pociatocna rychlost v smere hodu
+    %alfa_deg - [deg] uhol vrhu v stupnoch
 
-g = 9.81;        % [m/s^2] gravitacne zrychlenie  
-if alfa_deg > 90; alfa_deg=90; end;
-if alfa_deg < 0;  alfa_deg=0;  end;    
-alfa_rad = alfa_deg*2*pi/360;   % [rad] uhol vrhu v radianoch
+    % [m/s^2] gravitacne zrychlenie
+    g = 9.81;
+    if alfa_deg > 90; alfa_deg=90; end;
+    if alfa_deg < 0;  alfa_deg=0;  end;    
+    
+    % [rad] uhol vrhu v radianoch
+    alfa_rad = alfa_deg*2*pi/360;   
 
-td = 2*v0*sin(alfa_rad)/g       % [s] cas dopadu
-d  = v0^2*sin(2*alfa_rad)/g     % [m] dolet
-h  = v0^2*(sin(alfa_rad))^2/g/2 % [m] maximalna vyska
+    % [s] cas dopadu
+    td = 2*v0*sin(alfa_rad)/g;
+    
+    % [m] dolet
+    d  = v0^2*sin(2*alfa_rad)/g;
+    
+    % [m] maximalna vyska
+    h  = v0^2*(sin(alfa_rad))^2/g/2; 
 
-%sim('Sikmy_vrh_2');
+    % copy all function variable to master scope for projectile_sim
+    A = who;
+    for i = 1:length(A)
+        assignin('base', A{i}, eval(A{i}));
+    end
+end
 
-%figure(1);
-%plot(tout,vY,'b-','LineWidth',2); 
-%title('Vertikalna rychlost');
-%grid; xlabel('t [s]'); ylabel('v_y [m/s]');
-%yL = ylim; axis([0 td yL]);
 
-%figure(2);
-%plot(tout,Y,'b-','LineWidth',2); 
-%title('Vyska');
-%grid; xlabel('t [s]'); ylabel('y [m]');
-%yL = ylim; axis([0 td yL]);
 
-%figure(3);
-%plot(X,Y,'b-','LineWidth',2); 
-%title('Vyska vs Vzdialenost');
-%grid; xlabel('x [m]'); ylabel('y [m]');
-%yL = ylim; axis([0 d yL]);
--- test_sim.m
+++ test_sim.m
@@ -3,10 +3,21 @@
 %b = a.get('ScopeDataY')
 %assignin('base','b',b);
 
-A = [1.12, 2, 3, 4.12];
-B = [1.12, 2, 3, 4, 5, 6, 7, 8];
-C = setdiff(B, A)
+%A = [1.12, 2, 3, 4.12];
+%B = [1.12, 2, 3, 4, 5, 6, 7, 8];
+%C = setdiff(B, A)
 
 %ScopeDataVY(2,2);
 
-ScopeDataX(:,1)=[]
+%ScopeDataX(:,1)=[]
+
+%function y = test_sim(x, z)
+
+%x
+%z
+%y = x + z;
+%end
+
+% api = 'http://localhost:3000/';
+% url = [api 'simulation/init'];
+% S = webread(url)
